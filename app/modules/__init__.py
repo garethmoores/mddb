f8b4e65c70a97621f8a3bb54f280fe404d1a28da
Elliot Smith <elliot.smith91@gmail.com>
2015-10-24 12:50:00 +1000

    Added a simple endpoint to test indexing

--- app/modules/__init__.py
+++ app/modules/__init__.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from elasticsearch import Elasticsearch
+from rq import Queue
+from redis import Redis
 
 app = Flask(__name__)
+
+es = Elasticsearch()
+
+redis = Redis()
+q = Queue(connection=redis)
 
 import modules.hello
 
