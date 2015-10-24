9ebe5d0b28d2dd2bfe4e849ec468cf1cae582855
Matthew Brown <me@matthewbrown.io>
2015-10-24 19:54:54 +1000

    Added bower for front end dependencies. Flask-Secuirty for login

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 from elasticsearch import Elasticsearch
 from rq import Queue
 from redis import Redis
 import boto
 import os
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.mail import Mail
+from flask.ext.security import Security, SQLAlchemyUserDatastore
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+app.config['SECURITY_REGISTERABLE'] = True
+app.config['SECRET_KEY'] = 'randomsecrety1234'
 
+mail = Mail(app)
+db = SQLAlchemy(app)
 es = Elasticsearch()
 
 redis = Redis()
@@ -19,7 +28,12 @@ s3 = boto.connect_s3(ACCESS, KEY)
 bucket = s3.get_bucket('healthhack-mddb')
 
 import modules.hello
-import modules.indexing
+#import modules.indexing
+import modules.login
 
-if __name__ == "__main__":
-        app.run()
+user_datastore = SQLAlchemyUserDatastore(db, login.User, login.Role)
+security = Security(app, user_datastore)
+
+@app.route("/")
+def index():
+  return render_template("index.html")
--- modules/login.py
+++ modules/login.py
@@ -1,12 +1,37 @@
-from modules import app
-from flask import abort, make_response
-import jwt
+from modules import app, db
+from flask.ext.security import UserMixin, RoleMixin
 
-@app.route("/auth/token", methods=["POST"])
-def token():
-    username = request.form.get("username")
-    password = request.form.get("password")
-    if password is None and username is None:
-      abort(401)
 
-    encoded = jwt.encode({'username': username}, 'secret', algorithm='HS256')
+@app.before_first_request
+def create_user():
+    db.create_all()
+
+
+class Role(db.Model, RoleMixin):
+
+    __tablename__ = "roles"
+
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+
+roles_users = db.Table('roles_users',
+        db.Column('user_id', db.Integer(), db.ForeignKey('users.id')),
+        db.Column('role_id', db.Integer(), db.ForeignKey('roles.id')))
+
+
+class User(db.Model, UserMixin):
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
+
+    def __str__(self):
+        return '<User id=%s email=%s>' % (self.id, self.email)
